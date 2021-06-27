02d60b3d0a09ab1dc2f7b6a55d7284042ee2cb97
cybprom <ilerioluwac@gmail.com>
2021-06-26 19:57:26 -0500

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+/* // alert("This is an error!");
+var x = 15;
+var y = 10;
+document.write(x);
+var name = "ilerioluwa ";
+document.write(name);
+if (x > y) {
+  alert("THIS IS GREATER");
+} else {
+  alert("THIS IS LESSER");
+}
+
+function Myfunction() {
+  alert("This is a cookie");
+}
+
+Myfunction(); */
